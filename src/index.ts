6d64f2ad6e992d239483723252532c7cdb7eb066
Jerome <josejeromelalunio@gmail.com>
2021-12-08 06:17:36 +0800

    add stuff

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import mysql from "mysql";
+import util from "util";
+import {
+  apiQueryResponse,
+  apiResponse,
+  music,
+  musicProps,
+} from "./types/types";
 
 const app = express();
+app.use(express.json());
+
 const port = 3000;
 
-const connection = mysql.createConnection({
+export const connection = mysql.createConnection({
   host: "localhost",
   user: "jerome",
   password: "bdabccacbb",
@@ -13,16 +22,85 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-app.get("/music", (req, res) => {
-  connection.query(
-    `SELECT * FROM music WHERE mood = '${req.query.mood}'`,
-    (error, results, fields) => {
-      if (error) throw error;
-      res.send(results);
-    }
-  );
+app.get("/", (req, res) => {
+  res.send("Welcome to lofi moods api");
+});
+
+///       CREATE
+app.post("/addMusic", (req, res) => {
+  req.body.forEach((item: music) => insertIntoDb(item));
+
+  function insertIntoDb(item: music) {
+    connection.query(
+      `INSERT INTO music(title, owner, link, mood) VALUES('${item.title}', '${item.owner}', '${item.owner}', '${item.mood}')`,
+      (error, results, fields) => {
+        if (error) throw error;
+        res.send(results);
+      }
+    );
+  }
+});
+
+///       READ
+app.get("/getMusic", async (req, res) => {
+  let mood = req.query.mood as string;
+
+  let response: apiQueryResponse = await getMusic(mood);
+
+  let processedResponse: apiResponse<Array<music>>;
+  if (response.error) {
+    processedResponse = {
+      success: false,
+      message: "query failed",
+      data: response.results,
+    };
+  } else {
+    processedResponse = {
+      success: true,
+      message: "query success",
+      data: response.results,
+    };
+  }
+
+  res.send(processedResponse);
+
+  async function getMusic(mood: string): Promise<apiQueryResponse> {
+    let prom: Promise<apiQueryResponse> = new Promise((resolve) => {
+      connection.query(
+        `SELECT * FROM music WHERE mood = '${mood}'`,
+        (error, results, fields) =>
+          resolve({ error: error, results: results, fields: fields })
+      );
+    });
+
+    return prom;
+  }
+});
+
+///       UPDATE
+app.post("/updateMusic", (req, res) => {
+  console.log("got post");
+  console.log(req.body);
+
+  let item: music = req.body;
+
+  Object.keys(item).forEach((key: string, index: number) => {
+    updateColumn(key as musicProps, item);
+  });
+
+  function updateColumn(toUpdate: musicProps, item: music) {
+    util;
+    connection.query(
+      `UPDATE music SET ${toUpdate} = '${item[toUpdate]}' WHERE id = ${item.id}`,
+      (error, results, fields) => {
+        if (error) throw error;
+      }
+    );
+  }
+
+  res.send();
 });
 
 app.listen(port, () => {
-  console.log("listening");
+  console.log("listening on " + "http://localhost:" + port);
 });
---
+++ src/types/types.ts
@@ -0,0 +1,22 @@
+import { FieldInfo, MysqlError } from "mysql";
+
+export type musicProps = "id" | "title" | "owner" | "link" | "mood";
+export interface music {
+  id?: number;
+  title: string;
+  owner: string;
+  link: string;
+  mood: string;
+}
+
+export interface apiResponse<T> {
+  success: boolean;
+  message: string;
+  data: T;
+}
+
+export interface apiQueryResponse {
+  error: MysqlError | null;
+  results: any;
+  fields: Array<FieldInfo> | undefined;
+}
